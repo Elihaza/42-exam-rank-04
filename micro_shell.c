e2c0ff307cb8d39c6727e19a97111a2af8c20e82
ellarbi <ellarbi@student.42.fr>
2022-02-12 02:17:56 +0100

    finished microshell

--- micro_shell.c
+++ micro_shell.c
@@ -4,26 +4,22 @@
 #include <sys/wait.h>
 #include <string.h>
 
-void ft_putchar_err(char c)
-{
+void ft_putchar_err(char c) {
 	write(STDERR_FILENO, &c, 1);
 }
 
-int error(char* str)
-{
+int error(char* str) {
 	while (*str)
 		ft_putchar_err(*str++);
 	return (1);
 }
 
-int fatal(char** free_ptr)
-{
+int fatal(char** free_ptr) {
 	free(free_ptr);
 	exit(error("error: fatal\n"));
 }
 
-int size_cmd(char **cmd)
-{
+int size_cmd(char **cmd) {
 	if (!cmd)
 		return (0);
 	int i = -1;
@@ -32,8 +28,7 @@ int size_cmd(char **cmd)
 }
 
 // Return the size of **cmd until cmd[i] == str (useful for ";" and "|")
-int size_cmd_char(char **cmd, char *str)
-{
+int size_cmd_char(char **cmd, char *str) {
 	if (!cmd)
 		return (0);
 	int i = -1;
@@ -44,8 +39,7 @@ int size_cmd_char(char **cmd, char *str)
 }
 
 // Return a **char pointing just after the first pipe the func will meet
-char** find_next_pipe(char **cmd)
-{
+char** find_next_pipe(char **cmd) {
 	if (!cmd)
 		return (NULL);
 	int i = -1;
@@ -56,8 +50,7 @@ char** find_next_pipe(char **cmd)
 }
 
 // Return a **char containing a copy of av[i] until next ";"
-char** add_cmd(char **av, int *i)
-{
+char** add_cmd(char **av, int *i) {
 	int size = size_cmd_char(&av[*i], ";"); // size of new **char until next ";". We start from i position
 	if (!size)
 		return (NULL); // case ";" ";" with nothing between them
@@ -74,8 +67,7 @@ char** add_cmd(char **av, int *i)
 	return (tmp);
 }
 
-int builtin_cd(char **cmd)
-{
+int builtin_cd(char **cmd) {
 	if (size_cmd(cmd) != 2)
 		return (error("error: cd: bad arguments\n"));
 	if (chdir(cmd[1]) < 0)
@@ -88,8 +80,7 @@ int builtin_cd(char **cmd)
 }
 
 // Executes a command and free free_ptr in the forked process if an error occured
-int exec_cmd(char **cmd, char **env, char **free_ptr)
-{
+int exec_cmd(char **cmd, char **env, char **free_ptr) {
 	pid_t pid;
 
 	if ((pid = fork()) < 0)
@@ -111,8 +102,7 @@ int exec_cmd(char **cmd, char **env, char **free_ptr)
 // Do the pipes and then execute only the part of the command before next pipe.
 // free_ptr is char** cmd previously allocated. char** tmp isn't allocated,
 // it's just a ptr to **cmd so no need to free it.
-int exec_son(char** free_ptr, char** env, char** tmp, int fd_in, int fd_pipe[2])
-{
+int exec_son(char** free_ptr, char** env, char** tmp, int fd_in, int fd_pipe[2]) {
 	if (dup2(fd_in, STDIN_FILENO) < 0)
 		fatal(free_ptr);
 	if (find_next_pipe(tmp) && dup2(fd_pipe[1], STDOUT_FILENO) < 0) // If there is still a pipe after this command
@@ -133,12 +123,10 @@ int exec_son(char** free_ptr, char** env, char** tmp, int fd_in, int fd_pipe[2])
 	exit(0);
 }
 
-int execute(char **cmd, char **env)
-{
+int execute(char **cmd, char **env) {
 	/* CASE NO PIPES */
 	if (!find_next_pipe(cmd))
 		return (exec_cmd(cmd, env, cmd));
-
 	/* CASE PIPES */
 	int fd_in;
 	int fd_pipe[2];
@@ -151,16 +139,11 @@ int execute(char **cmd, char **env)
 
 	while (tmp)
 	{
-		if (pipe(fd_pipe) < 0 || (pid = fork()) < 0)
+		if (pipe(fd_pipe) < 0 || (pid = fork()) < 0) // Son is executing commands
 			fatal(cmd);
-			
-		// Son is executing commands
 		if (!pid)
 			exec_son(cmd, env, tmp, fd_in, fd_pipe);
-		
-		// Parent is just saving fd_pipe[0] for next son execution and correctly closing pipes
-		else
-		{
+		else { // Parent is just saving fd_pipe[0] for next son execution and correctly closing pipes
 			if (dup2(fd_pipe[0], fd_in) < 0)	// Really important to protect syscalls using fd,
 				fatal(cmd);						// tests with wrong fds will be done during grademe
 			close(fd_pipe[0]);
@@ -169,12 +152,8 @@ int execute(char **cmd, char **env)
 			tmp = find_next_pipe(tmp); // Goes to the next command to be executed, just after first pipe met
 		}
 	}
-
-	//closing last dup2 that happen in the last parent loop tour
-	close(fd_in); 
-
-	//waiting for each command launched to bed executed
-	while (nb_wait-- >= 0)
+	close(fd_in); //closing last dup2 that happen in the last parent loop tour
+	while (nb_wait-- >= 0) //waiting for each command launched to bed executed
 		waitpid(0, NULL, 0);
 	return (0);
 }
